09050783a7eda14c1a0ab0f05e08f7a6d48bc5d8
Tamayaren <tamayaren@proton.me>
2026-07-10 22:44:50 +0800

    test diff

--- sum.js
+++ sum.js
@@ -1,6 +1,11 @@
 function sum(a, b) {
     console.log("connetion change:");
+    console.log("test commit");
     return a + b;
+}
+
+function diff(a, b) {
+    return a - b;
 }
 
 module.exports = sum
